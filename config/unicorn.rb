598593e7823e2a749c6447474733ea53552c53de
fushang318 <fushang318@gmail.com>
2010-09-21 13:43:35 +0800

    增加 unicorn 日志

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,9 @@ worker_processes 3
 listen '/tmp/nginx/sockets/config_manager_unicorn.sock', :backlog => 2048
 timeout 30
 
+stderr_path("#{File.dirname(__FILE__)}/../log/unicorn_error.log")
+stdout_path("#{File.dirname(__FILE__)}/../log/unicorn.log")
+
 pid_file = "/web/2010/pids/unicorn_pin_config_manager.pid"
 pid pid_file
 preload_app true
